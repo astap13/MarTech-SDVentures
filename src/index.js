8d5a79b1a45af7cbf5c3e6f0eff05944ce437cdd
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-09-30 01:12:33 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import avatarImage from './assets/avatars.png'; 
 import './styles.scss';
+import { openModal, closeModal } from './modules/modal.js';
+
 
 const imgElement = document.querySelector('.img-container img');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+
 imgElement.src = avatarImage;
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
---
+++ src/modules/modal.js
@@ -0,0 +1,10 @@
+export function openModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'flex';
+  }
+  
+export function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+  
